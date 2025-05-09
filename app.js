a8271fca28fde76a07e09de747f13f15289a59d3
martinolanza03 <lanzamartino3@gmail.com>
2025-05-09 15:35:18 +0200

    feat: create middleware

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const movieRouter = require('./routers/movieRouter');
 
+//Middleware
+const errorsHandler = require('./middleware/errorHandler.js');
+const notFound = require('./middleware/notFound.js');
+
 //Call routes in movieRouter.js
 app.use('/movies', movieRouter);
+
+
+//Call the middleware
+//Route Not Found
+app.use(notFound);
+
+//Error in the code
+app.use(errorsHandler);
 
 //Server port
 app.listen(port, () => {
